9be628f1c0297f8cd16570dd00f043374ee32a6d
CHAOSdonut <rick2.0@hotmail.nl>
2022-12-02 11:21:19 +0100

    day 2

---
+++ day2/day2.js
@@ -0,0 +1,74 @@
+import fs from "fs";
+
+const testInput =
+    `A Y
+B X
+C Z`
+
+const input = fs.readFileSync("../input/day2Input", "utf-8");
+
+let matchArray = [];
+const data = input.split('\r\n');
+data.forEach(value => {
+    matchArray.push(value.split(' '))
+})
+
+let score1 = 0;
+function playMatch1(p1, p2) {
+    switch (p2) {
+        case 'X':
+            score1 += 1
+            break;
+        case 'Y':
+            score1 += 2
+            break;
+        case 'Z':
+            score1 += 3
+            break;
+        default:
+
+    }
+
+    if (p1 === "A" && p2 === "X"){score1 += 3}
+    if (p1 === "A" && p2 === "Y"){score1 += 6}
+    if (p1 === "A" && p2 === "Z"){score1 += 0}
+
+    if (p1 === "B" && p2 === "X"){score1 += 0}
+    if (p1 === "B" && p2 === "Y"){score1 += 3}
+    if (p1 === "B" && p2 === "Z"){score1 += 6}
+
+    if (p1 === "C" && p2 === "X"){score1 += 6}
+    if (p1 === "C" && p2 === "Y"){score1 += 0}
+    if (p1 === "C" && p2 === "Z"){score1 += 3}
+}
+
+let score2 = 0;
+function playMatch2(p1, p2) {
+    switch (p2) {
+        case 'X':
+            if (p1 === "A"){score2 += (3 + 0)}
+            if (p1 === "B"){score2 += (1 + 0)}
+            if (p1 === "C"){score2 += (2 + 0)}
+            break;
+        case 'Y':
+            if (p1 === "A"){score2 += (1 + 3)}
+            if (p1 === "B"){score2 += (2 + 3)}
+            if (p1 === "C"){score2 += (3 + 3)}
+            break;
+        case 'Z':
+            if (p1 === "A"){score2 += (2 + 6)}
+            if (p1 === "B"){score2 += (3 + 6)}
+            if (p1 === "C"){score2 += (1 + 6)}
+            break;
+        default:
+
+    }
+}
+
+matchArray.forEach(value => {
+    playMatch1(value[0], value[1])
+    playMatch2(value[0], value[1])
+})
+
+console.log(`Total score game 1: ${score1}`);
+console.log(`Total score game 2: ${score2}`);
