b6106903f5f312a28e5045ce0b91a6cc29f6c6a1
CHAOSdonut <rick2.0@hotmail.nl>
2022-12-02 11:31:30 +0100

    cleanup day 2

--- day2/day2.js
+++ day2/day2.js
@@ -18,47 +18,47 @@ function playMatch1(p1, p2) {
     switch (p2) {
         case 'X':
             score1 += 1
+            if (p1 === "A"){score1 += 3}
+            if (p1 === "B"){score1 += 0}
+            if (p1 === "C"){score1 += 6}
             break;
         case 'Y':
             score1 += 2
+            if (p1 === "A"){score1 += 6}
+            if (p1 === "B"){score1 += 3}
+            if (p1 === "C"){score1 += 0}
             break;
         case 'Z':
             score1 += 3
+            if (p1 === "A"){score1 += 0}
+            if (p1 === "B"){score1 += 6}
+            if (p1 === "C"){score1 += 3}
             break;
         default:
 
     }
-
-    if (p1 === "A" && p2 === "X"){score1 += 3}
-    if (p1 === "A" && p2 === "Y"){score1 += 6}
-    if (p1 === "A" && p2 === "Z"){score1 += 0}
-
-    if (p1 === "B" && p2 === "X"){score1 += 0}
-    if (p1 === "B" && p2 === "Y"){score1 += 3}
-    if (p1 === "B" && p2 === "Z"){score1 += 6}
-
-    if (p1 === "C" && p2 === "X"){score1 += 6}
-    if (p1 === "C" && p2 === "Y"){score1 += 0}
-    if (p1 === "C" && p2 === "Z"){score1 += 3}
 }
 
 let score2 = 0;
 function playMatch2(p1, p2) {
     switch (p2) {
         case 'X':
-            if (p1 === "A"){score2 += (3 + 0)}
-            if (p1 === "B"){score2 += (1 + 0)}
-            if (p1 === "C"){score2 += (2 + 0)}
+            score2 += 0
+            if (p1 === "A"){score2 += 3}
+            if (p1 === "B"){score2 += 1}
+            if (p1 === "C"){score2 += 2}
             break;
         case 'Y':
-            if (p1 === "A"){score2 += (1 + 3)}
-            if (p1 === "B"){score2 += (2 + 3)}
-            if (p1 === "C"){score2 += (3 + 3)}
+            score2 += 3
+            if (p1 === "A"){score2 += 1}
+            if (p1 === "B"){score2 += 2}
+            if (p1 === "C"){score2 += 3}
             break;
         case 'Z':
-            if (p1 === "A"){score2 += (2 + 6)}
-            if (p1 === "B"){score2 += (3 + 6)}
-            if (p1 === "C"){score2 += (1 + 6)}
+            score2 += 6
+            if (p1 === "A"){score2 += 2}
+            if (p1 === "B"){score2 += 3}
+            if (p1 === "C"){score2 += 1}
             break;
         default:
 
